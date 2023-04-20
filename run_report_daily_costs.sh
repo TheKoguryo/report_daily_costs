c822393c1649b0f902db356a89c5939f03eb7e6c
Oracle Public Cloud User <opc@usage-vm-8.sub09010718400.ociusagevcn.oraclevcn.com>
2023-04-20 10:52:24 +0000

    crontab test

--- run_report_daily_costs.sh
+++ run_report_daily_costs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+export APPDIR=$HOME/report_daily_costs
+export TENANT_ID=ocid1.tenancy.oc1..aaaaaaaa.....
+export ONS_TOPIC_ID=ocid1.onstopic.oc1.iad.aaaaaaaa.....
+
+cd $APPDIR
+
 ###########################################################
 # Main
 ###########################################################
 echo "Start running at `date`..."
 
-python3 report_daily_costs.py --tenant_id $TENANT_ID --ons_topic_id $ONS_TOPIC_ID
+python3 $APPDIR/report_daily_costs.py --tenant_id $TENANT_ID --ons_topic_id $ONS_TOPIC_ID
 
 echo "Completed at `date`.."
