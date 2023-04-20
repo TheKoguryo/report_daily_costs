839f837f5e276bb9af8d30d5c18e1b04c327a4ca
TheKoguryo <thekooguryo@gmail.com>
2023-04-20 18:58:14 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # report-daily-costs
+
+## OCI CLI usage-api example
+
+```
+oci usage-api usage-summary request-summarized-usages --tenant-id ocid1.tenancy.oc1..aaaaaaaa.....xxxxx --time-usage-started 2023-04-19T06:00 --time-usage-ended 2023-04-19T07:00 --granularity HOURLY
+
+oci usage-api usage-summary request-summarized-usages --tenant-id ocid1.tenancy.oc1..aaaaaaaa.....xxxxx --time-usage-started 2023-04-19T05:00 --time-usage-ended 2023-04-19T06:00 --granularity HOURLY --group-by ["service"]
+
+oci usage-api usage-summary request-summarized-usages --tenant-id ocid1.tenancy.oc1..aaaaaaaa.....xxxxx --time-usage-started 2023-04-19T00:00 --time-usage-ended 2023-04-20T00:00 --granularity DAILY
+
+oci usage-api usage-summary request-summarized-usages --tenant-id ocid1.tenancy.oc1..aaaaaaaa.....xxxxx --time-usage-started 2023-04-19T00:00 --time-usage-ended 2023-04-20T00:00 --granularity DAILY --group-by ["service"]
+```
---
+++ report-daily-costs.py
@@ -0,0 +1,109 @@
+import oci
+import logging
+import datetime
+import argparse
+
+def report_daily_costs(tenant_id, ons_topic_id):
+    identity_client = oci.identity.IdentityClient(config)
+    notification_client = oci.ons.NotificationDataPlaneClient(config)
+    usage_api_client = oci.usage_api.UsageapiClient(config)
+
+    now = datetime.datetime.now()
+    yesterday = now - datetime.timedelta(days=1)
+    time_usage_started = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+    time_usage_ended = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    one_day_ago_amount = 0
+    two_days_ago_amount = 0
+
+    ## Yesterday
+    data = usage_api_client.request_summarized_usages(
+                request_summarized_usages_details=oci.usage_api.models.RequestSummarizedUsagesDetails(
+                    tenant_id=tenant_id,
+                    time_usage_started=time_usage_started,
+                    time_usage_ended=time_usage_ended,
+                    granularity="DAILY"
+                )
+            ).data
+    
+    for item in data.items:
+        if item.computed_amount is None:
+            continue
+        
+        one_day_ago_amount += item.computed_amount
+        currency = item.currency
+
+    ## The day before yesterday
+    data = usage_api_client.request_summarized_usages(
+                request_summarized_usages_details=oci.usage_api.models.RequestSummarizedUsagesDetails(
+                    tenant_id=tenant_id,
+                    time_usage_started=time_usage_started - datetime.timedelta(days=1),
+                    time_usage_ended=time_usage_ended - datetime.timedelta(days=1),
+                    granularity="DAILY"
+                )
+            ).data
+    
+    for item in data.items:
+        if item.computed_amount is None:
+            continue
+        
+        two_days_ago_amount += item.computed_amount
+
+    difference = (one_day_ago_amount / two_days_ago_amount) * 100 - 100
+
+    # Notification
+    title =""
+    tenancy = None
+    if signer is not None:
+        tenancy = identity_client.get_tenancy(signer.tenancy_id).data
+    else:
+        tenancy = identity_client.get_tenancy(config["tenancy"]).data
+
+    if difference > 0:
+        title = "[" + f'{difference:,.2f}' + "% Up]"
+    elif difference == 0:
+        title = "[No Difference]"
+    else:
+        title = "[" + f'{difference:,.2f}' + "% Down]"
+
+    title += " Tenancy " + tenancy.name + ": Daily Cost Report (" + time_usage_started.strftime('%m') + "/" + time_usage_started.strftime('%d') + ") - "
+    title += currency + " " + f'{one_day_ago_amount:,.0f}'
+    title += " (EOM)"
+
+    logging.getLogger().info("ons_topic_id: " + ons_topic_id)
+
+    notification_message = {"title": title, "body": " "}
+    logging.getLogger().info("notification_message: " + str(notification_message))
+    notification_client.publish_message(ons_topic_id, notification_message)    
+
+def prep_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tenant_id', default='',
+                        help='The tenant where you want to get a daily cost report')
+    parser.add_argument('--ons_topic_id', default='',
+                        help='The notification topic id where you want to publish a message for notifying ')    
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('oci._vendor.urllib3').setLevel(logging.INFO)
+
+    # Starts a timer for the execution time.
+    logging.getLogger().info('Report Daily Costs v 0.1')
+    start_time = datetime.datetime.now()
+    logging.getLogger().info('Script execution start time: {0}'.format(
+        start_time.replace(microsecond=0).isoformat()))    
+
+    # Default config file and profile
+    config = oci.config.from_file()
+
+    signer = None
+    #signer = oci.auth.signers.get_resource_principals_signer()
+    #signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
+        
+    args = prep_arguments()
+    tenant_id = args.tenant_id
+    ons_topic_id = args.ons_topic_id
+
+    report_daily_costs(tenant_id, ons_topic_id)
