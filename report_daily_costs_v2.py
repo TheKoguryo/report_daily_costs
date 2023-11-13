0fec4debb73b53f45f82e05dd99307990640c6dd
TheKoguryo <thekooguryo@gmail.com>
2023-11-13 09:07:40 +0900

    v2 업데이트

--- report_daily_costs_v2.py
+++ report_daily_costs_v2.py
@@ -379,7 +379,6 @@ def generate_report(tenant_id, d_day_started):
     html += "<head>\n"
     html += "	<meta charset='UTF-8'>\n"
     html += "	<meta http-equiv='X-UA-Compatible' content='IE=edge'>\n"
-    html += "	<meta name='viewport' content='width=device-width, initial-scale=1'>\n"
     html += "	<title>OCI Daily Costs Report</title>\n"
     html += "	<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css' integrity='sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu' crossorigin='anonymous'>\n"
     html += "	<style>\n"
